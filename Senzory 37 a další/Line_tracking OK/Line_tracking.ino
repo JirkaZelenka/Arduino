f7323ac17c970bba94d418d61c819ab4f1737715
JirkaZelenka <43747280+JirkaZelenka@users.noreply.github.com>
2020-05-04 02:28:54 +0200

    Add files via upload

---
+++ Senzory 37 a další/Line_tracking OK/Line_tracking.ino
@@ -0,0 +1,9 @@
+void setup()
+{
+  Serial.begin(9600);
+}
+void loop()
+{
+  Serial.println(digitalRead(2)); // print the data from the sensor
+  delay(500);
+}
---
+++ Senzory 37 a další/Photoresistor OK/Photoresistor.ino
@@ -0,0 +1,19 @@
+//KY018 Photo resistor module
+
+// 0 = Super světlo, 1000 = super tma !!!
+ 
+int sensorPin = A5; // select the input pin for the potentiometer
+int ledPin = 13; // select the pin for the LED
+int sensorValue = 0; // variable to store the value coming from the sensor
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  digitalWrite(ledPin, HIGH);
+  delay(sensorValue);
+  digitalWrite(ledPin, LOW);
+  delay(sensorValue);
+  Serial.println(sensorValue, DEC);
+}
